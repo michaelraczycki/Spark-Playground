2924f2b5e6b8f451d9a65e8f5198ee63b9dc0107
Michal Raczycki <michal.raczycki@externals.adidas.com>
2024-10-18 10:30:27 +0200

    switching to camel case, updating pre-commit

---
+++ exercises/spark_streaming.py
@@ -0,0 +1,64 @@
+import json
+
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, date_format, window
+
+# Load the configuration file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# Get the data path from the configuration
+dataPath = config['data_path']
+
+# Initialize Spark session
+spark = SparkSession.builder.master("local").appName("sparkStreaming").getOrCreate()
+spark.conf.set("spark.sql.shuffle.partitions", 5)
+
+# Load data
+staticDataFrame = spark.read.format("csv")\
+    .option("header", "true")\
+    .option("inferSchema", "true")\
+    .load(f"{dataPath}/retail-data/by-day/*.csv")
+
+# Create temporary view and use schema
+staticDataFrame.createOrReplaceTempView("retailData")
+staticSchema = staticDataFrame.schema
+
+# Perform select and group by operations
+staticDataFrame.selectExpr("CustomerId", "(UnitPrice * Quantity) as totalCost", "InvoiceDate")\
+    .groupBy(col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
+    .sum("totalCost")\
+    .show(5)
+
+# Print schema and prepare the data
+staticDataFrame.printSchema()
+preppedDataFrame = staticDataFrame.na.fill(0)\
+    .withColumn("dayOfWeek", date_format(col("InvoiceDate"), "EEEE")).coalesce(5)
+print(preppedDataFrame.take(2))
+
+# Prepare the pipeline:
+
+trainDataFrame = preppedDataFrame.where("InvoiceDate < '2011-07-01'")
+testDataFrame = preppedDataFrame.where("InvoiceDate >= '2011-07-01'")
+
+indexer = StringIndexer()\
+    .setInputCol("dayOfWeek")\
+    .setOutputCol("dayOfWeekIndex")
+
+encoder = OneHotEncoder()\
+    .setInputCol("dayOfWeekIndex")\
+    .setOutputCol("dayOfWeekEncoded")
+
+vectorAssembler = VectorAssembler()\
+    .setInputCols(["UnitPrice", "Quantity", "dayOfWeekEncoded"])\
+    .setOutputCol("features")
+
+transformationPipeline = Pipeline()\
+    .setStages([indexer, encoder, vectorAssembler])
+
+fittedPipeline = transformationPipeline.fit(trainDataFrame)
+transformedTraining = fittedPipeline.transform(trainDataFrame)
+
+transformedTraining.cache()
